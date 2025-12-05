dc20701934f1459059af3ae328d5e558280bcba5
keonwoo98 <keonwoo98@naver.com>
2025-12-05 19:09:22 +0100

    Add Hardhat configuration, package.json, and package-lock.json for BEP-20 token project

---
+++ code/hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      // 로컬 테스트용 네트워크
+    },
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.BSCSCAN_API_KEY || "",
+    },
+  },
+};
